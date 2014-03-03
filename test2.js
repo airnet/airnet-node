94b5ee0bf0757d57630abbec732f4dec54478938
Christian Stewart <kidovate@gmail.com>
2014-03-03 12:19:11 -0800

    Updated tests and added verification output

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 airnet = require('./index.js');
 
-node = new airnet(3001, {file: "data2.airnet", ip: "54.186.25.188"});
+node = new airnet(3001, {file: "data2.airnet"});
 node.start();
 node.connect('localhost:3000');
-node.connect('172.31.15.243:3000');
+node.connect('54.186.25.188:3000');
