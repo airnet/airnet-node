e5a679e11aa4e5d9af776ddbd59f1806a2fe81b7
Christian Stewart <kidovate@gmail.com>
2014-03-03 12:11:39 -0800

    Save peers and connect to them on start

--- test2.js
+++ test2.js
@@ -1,5 +1,6 @@
 airnet = require('./index.js');
 
-node = new airnet(3001, {file: "data2.airnet"});
+node = new airnet(3001, {file: "data2.airnet", ip: "54.186.25.188"});
 node.start();
 node.connect('localhost:3000');
+node.connect('172.31.15.243:3000');
